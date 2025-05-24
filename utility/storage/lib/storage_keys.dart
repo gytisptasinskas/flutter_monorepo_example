7a390dfee03b0d0f194c522a8ca7776e7850f472
gytisptasinskastelesoftas <51530980+gytisptasinskas@users.noreply.github.com>
2025-05-24 21:18:10 +0300

    feat: update dependency overrides and add event_bus path; enhance storage keys

--- utility/storage/lib/storage_keys.dart
+++ utility/storage/lib/storage_keys.dart
@@ -1,4 +1,6 @@
 class StorageKeys {
   static const String isLoggedIn = 'isLoggedIn';
   static const String userName = 'userName';
+  static const String userEmail = 'userEmail';
+  static const String userId = 'userId';
 }
