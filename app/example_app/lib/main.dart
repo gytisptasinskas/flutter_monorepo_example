8de4ef2b3b791d6c8d99d20f802f0a41ace7a091
gytisptasinskastelesoftas <51530980+gytisptasinskas@users.noreply.github.com>
2025-05-24 19:52:54 +0300

    feat: update main entry point and enhance dependency overrides in configuration files

--- app/example_app/lib/main.dart
+++ app/example_app/lib/main.dart
@@ -1,18 +1,6 @@
+import 'package:example_app/application.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) => MaterialApp(
-    title: 'Flutter Demo',
-    theme: ThemeData(
-      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-    ),
-    home: const SizedBox(),
-  );
+  runApp(const Application());
 }
